5d106e4d07443f06a77bcd7a3d1db53b3ab397dc
mbongeniyika-arch <mbongeniyika@icloud.com>
2026-03-04 20:22:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// document.getElementById("loanForm").addEventListener("submit", function(e) {
+//     e.preventDefault();
+
+//     emailjs.sendForm(
+//         "service_w7qw24j",
+//         "template_z5x9lyn",
+//         this
+//     ).then(function() {
+//         document.getElementById("message").innerText = 
+//             "Application submitted successfully!";
+//     }, function(error) {
+//         document.getElementById("message").innerText = 
+//             "Failed to send application. Try again.";
+//     });
+// });
